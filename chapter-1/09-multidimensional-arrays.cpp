c6abe21bef94800403af8d475c54ef986f09a263
ndevbogado <ndevbogado@gmail.com>
2022-11-07 22:34:01 -0300

    Create multidimensional arrays 09-practice-file

---
+++ chapter-1/09-multidimensional-arrays.cpp
@@ -0,0 +1,4 @@
+//Alphabetize a string input. Remember that the value of the characters increase from 'a'
+//Sort a randomized 2D array of integers using whichever algorithm you want. You should order the rows by their first (minimum) elements.
+//Read in a 2D array of c-strings from a file and print them into the screen. Aditionally, try sorting them alphabetically, and transferring the data to a one-dimensional array of c-strings.
+
