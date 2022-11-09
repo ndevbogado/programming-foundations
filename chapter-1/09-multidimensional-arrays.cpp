7b8667612b6590a565fbe96a033f1aad941fcc50
ndevbogado <ndevbogado@gmail.com>
2022-11-09 16:11:05 -0300

    Add randomNumber() and for loop to test

--- chapter-1/09-multidimensional-arrays.cpp
+++ chapter-1/09-multidimensional-arrays.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <fstream>
+// for rand() usage
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int randomNumber(bool &createSeed);
+
+int main () {
+	bool createSeed = true;
+	for ( int i = 0 ; i < 5 ; i++) {
+		cout<<randomNumber(createSeed)<<" ";
+	}
+	cout<<endl;
+}
+
+int randomNumber(bool &createSeed){
+	if (createSeed) {
+		srand(time(0));
+		createSeed = false;
+		cout<<"Seed created successfully!"<<endl;
+	}	
+	return rand()%10;
+}
+
 //Alphabetize a string input. Remember that the value of the characters increase from 'a'
 //Sort a randomized 2D array of integers using whichever algorithm you want. You should order the rows by their first (minimum) elements.
 //Read in a 2D array of c-strings from a file and print them into the screen. Aditionally, try sorting them alphabetically, and transferring the data to a one-dimensional array of c-strings.
