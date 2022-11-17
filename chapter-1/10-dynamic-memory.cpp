f9ea43870dcb087eccb0cc9f28ea45c8f39a9cd2
ndevbogado <ndevbogado@gmail.com>
2022-11-17 17:04:17 -0300

    feat: Add TODO list of practice work

---
+++ chapter-1/10-dynamic-memory.cpp
@@ -0,0 +1,19 @@
+/*
+
+1-    Create a program to input and display a number of values from a file (specified in the file), and only store as much data as you need to.
+
+2-    Implement a sorting function that will sort an array into a new, dynamically allocated array.
+
+3-    Try making and using a multidimensional, dynamically allocated array.
+
+4-    Try creating these same c-style string functions, but with with dynamically allocated, exactly sized strings. Consider how this affects your functions. Do you need to resize strings? When do you need to allocate new memory? When do you need to pass pointers by reference?
+        String copy: copies one c-string into another
+        String concat: adds one string onto the end of another
+        String length: returns the length of a string
+        Substring: returns the position of a substring within a string
+        String insert: inserts a string into another string
+        String replace: inserts a string into another string but doesn’t push back the following characters.
+        String fill: fills a string with a character from a starting position to an ending position.
+        String reverse: flips a string backwards.
+
+*/
